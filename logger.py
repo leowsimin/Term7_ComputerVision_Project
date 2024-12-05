dbb1494a174cb17d57be518842d0cc68690b4757
SausageMuffins <rwang9098@gmail.com>
2024-12-05 16:48:03 +0800

    modified logger to log the best checkpoint's name for ease later on

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import config
 import tensorflow as tf
 
-# create folder 'logs' if not
+# Create folder 'logs' if not exists
 folder_name = "logs"
 if not os.path.exists(folder_name):
     os.makedirs(folder_name)
@@ -11,21 +11,21 @@ if not os.path.exists(folder_name):
 else:
     print(f"Folder '{folder_name}' already exists.")
 
-# get TF logger
+# Get TensorFlow logger
 tf_logger = tf.get_logger()
 tf_logger.setLevel(logging.DEBUG)
 
-# create formatter and add it to the handlers
+# Create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# create file handler which logs even debug messages
+# Create file handler which logs even debug messages
 filename = f"{datetime.today().strftime('%Y_%m_%d_%H_%M_%S')}.log"
 fh = logging.FileHandler(f'logs/{filename}')
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
 tf_logger.addHandler(fh)
 
-# log config values
+# Log config values
 tf_logger.info("Config:")
 for k, v in vars(config).items():
     if k[:2] != '__':
