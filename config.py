a10f46e946917d7e0a8a7c127cdcf9c20de3fb2a
SausageMuffins <rwang9098@gmail.com>
2024-12-05 17:18:07 +0800

    tested my own trained model

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ total_epoch = 100 # added more epochs to see if the model can improve - gradient
 dataset = "lsp" # "lspet"
 
 # Train mode: 0-heatmap, 1-regression
-train_mode = 0
+train_mode = 1
 
 # Eval mode: 0-output image, 1-pck score
 eval_mode = 1
 
 continue_train = 0
 
-best_pre_train = None # num of epoch where the training loss drops but testing accuracy achieve the optimal
+best_pre_train = 98 # num of epoch where the training loss drops but validation accuracy achieve the lowest
 
 # for test only
-epoch_to_test = 55
-use_existing_model_weights = 1
+epoch_to_test = 66
+use_existing_model_weights = 0
