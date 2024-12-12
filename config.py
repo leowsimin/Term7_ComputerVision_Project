c9b24eb5b87dd32aeb1a404a76503c4d1460519d
leowsimin <leowsmr@gmail.com>
2024-12-13 03:28:29 +0800

    new changes

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ train_split, val_split, test_split = 0.6, 0.2, 0.2 # split
 heat_size = 64
 
 # Train mode: 0-heatmap, 1-regression
-train_mode = 0
+train_mode = 1
 
 # Eval mode: 0-output image, 1-pck score
 eval_mode = 1
@@ -16,8 +16,8 @@ pck_metric = 0.5 # standard; point is correct if distance to gt < 50% of person'
 img_idxs=[1800, 1906, 1981, 1995] # which images to draw when predicting
 
 continue_train = 0
-continue_train_from_filename = "model_ep20.weights.h5"
-best_pre_train_filename = ""
+continue_train_from_filename = ""
+best_pre_train_filename = "model_ep50.weights.h5"
 
 # for test only
 epoch_to_test = 55
